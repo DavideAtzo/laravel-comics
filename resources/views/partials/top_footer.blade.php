4071a73d75ac2ad1be1ab1ffafa7dbca610dd08d
DavideAtzo <atzo9477@gmail.com>
2023-05-03 16:56:29 +0200

    top footer finito

--- resources/views/partials/top_footer.blade.php
+++ resources/views/partials/top_footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="row">
             <ul class="list-unstyled d-flex justify-content-between align-items-center gap-5 py-3">
-                <li><a href="#">DIGITAL COMICS</a> </li>
-                <li><a href="#">DC MERCHANDISE</a></li>
-                <li><a href="#">SUBSCRIPTION</a></li>
-                <li><a href="#">COMIC SHOP LOCATOR</a></li>
-                <li><a href="#">DC POWER VISA</a></li>
+                <li><a href="#"><img class="px-2" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="COMICS"> DIGITAL COMICS</a> </li>
+                <li><a href="#"><img class="px-2" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="COMICS">DC MERCHANDISE</a></li>
+                <li><a href="#"><img class="px-2" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="COMICS">SUBSCRIPTION</a></li>
+                <li><a href="#"><img class="px-2" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="COMICS">COMIC SHOP LOCATOR</a></li>
+                <li><a href="#"><img class="px-2" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="COMICS">DC POWER VISA</a></li>
             </ul>
         </div>
     </div>
