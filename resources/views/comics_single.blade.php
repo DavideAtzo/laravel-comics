4cd825f570cb0dc17cd7639878e2192c5d9dd089
DavideAtzo <atzo9477@gmail.com>
2023-05-03 19:43:25 +0200

    inserimento publicità

--- resources/views/comics_single.blade.php
+++ resources/views/comics_single.blade.php
@@ -15,16 +15,26 @@
 <section id="descrizione_prodotto">
     <div class="container">
         <div class="row">
-            <div class="title">
-                <h1>{{ $comics_single['title'] }}</h1>
-            </div>
-            <div class="row">
-                <div class="col-9 d-flex">
-                    <div class="col">U.S.Price: <span>{{ $comics_single['price'] }}</span></div>
-                    <div class="col-1">available</div>
+            <div class="col-8">
+                <div class="row mt-5">
+                    <div class="title">
+                        <h1>{{ $comics_single['title'] }}</h1>
+                    </div>
                 </div>
-                <div class="col-3">
-                    Check Availability
+                <div class="row price py-2 justify-content-between">
+                    <div class="col-9 d-flex">
+                        <div class="col">U.S.Price: <span>{{ $comics_single['price'] }}</span></div>
+                        <div class="col-1 text-uppercase">available</div>
+                    </div>
+                    <div class="col-3 text-center">
+                        Check Availability
+                    </div>
+                </div>
+            </div>
+            <div class="col-4  mt-3">
+                <h3 class="text-uppercase text-center ps-5">advertisement</h3>
+                <div class="pubblicità px-3">
+                    <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="jumbotron">
                 </div>
             </div>
         </div>
