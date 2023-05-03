d0903e04942c910823befbe944b8d9433d4b3e6b
DavideAtzo <atzo9477@gmail.com>
2023-05-03 09:41:40 +0200

    badge current and load more

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,9 @@
 @section('page.main')
 <section id="posters">
     <div class="container">
+        <div class="btn btn-primary">
+            <h4>CURRENT SIRIES</h4>
+        </div>
         <div class="poster_list d-flex">
              @foreach ($comics as $item)
                 <div class="card">
@@ -12,6 +15,9 @@
                     </div>
                 </div>
             @endforeach
+        </div>
+        <div class="btn btn-primary">
+            <h4>LOAD MORE</h4>
         </div>
     </div>
 </section>
