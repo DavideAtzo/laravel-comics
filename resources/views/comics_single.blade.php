b2353b27589be353907299c873f69337f2db4fb0
DavideAtzo <atzo9477@gmail.com>
2023-05-03 22:06:11 +0200

    sezione more links sul dettaglio prodotto

--- resources/views/comics_single.blade.php
+++ resources/views/comics_single.blade.php
@@ -43,16 +43,16 @@
         </div>
     </div>
 </section>
-<section id="more-info" class="mt-3">
+<section id="more-info" class="py-5">
     <div class="container">
         <div class="row">
             <div class="col-6">
-                <div class="row">
+                <div class="row border-bottom">
                     <div>
                         <h3 class="py-3">Talent</h3>
                     </div>
                 </div>
-                <div class="row justify-content-between py-2">
+                <div class="row justify-content-between py-2 border-bottom">
                     <div class="col-4"><h6>Art by:</h6></div>
                     <div class="col-8">
                         @foreach ($comics_single['artists'] as $artist)
@@ -60,7 +60,7 @@
                          @endforeach
                     </div>
                 </div>
-                <div class="row py-2">
+                <div class="row py-2 border-bottom">
                     <div class="col-4"><h6>Written by:</h6></div>
                     <div class="col-8">
                         @foreach ($comics_single['writers'] as $writer)
@@ -70,24 +70,62 @@
                 </div>
             </div>
             <div class="col-6">
-                <div class="row">
+                <div class="row border-bottom">
                     <div>
                         <h3 class="py-3">Specs</h3>
                     </div>
                 </div>
-                <div class="row py-2">
+                <div class="row py-2 border-bottom">
                     <div class="col-4"><h6>Series:</h6></div>
                     <div class="col-8">
                         <a href="#">{{ $comics_single['series'] }}</a>
                     </div>
                 </div>
-                <div class="row py-2">
+                <div class="row py-2 border-bottom">
                     <div class="col-4"><h6>U.S. Price:</h6></div>
                     <div class="col-8">{{ $comics_single['price'] }}</div>
                 </div>
-                <div class="row py-2">
+                <div class="row py-2 border-bottom">
                     <div class="col-4"><h6>On Sale Date:</h6></div>
                     <div class="col-8">{{ $comics_single['sale_date'] }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="more-links" class="border">
+    <div class="container">
+        <div class="row">
+            <div class="col-3 d-flex justify-content-between border">
+                <div>
+                    <h5 class="text-uppercase py-3">Digital Comics</h5>
+                </div>
+                <div class="py-2">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="Digital Comics">
+                </div>
+            </div>
+            <div class="col-3 d-flex justify-content-between border">
+                <div>
+                    <h5 class="text-uppercase py-3">shop dc</h5>
+                </div>
+                <div class="py-2">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="Shop DC">
+                </div>
+            </div>
+            <div class="col-3 d-flex justify-content-between border">
+                <div class="py-2">
+                    <h5 class="text-uppercase py-3">comic shop locator</h5>
+                </div>
+                <div class="py-2">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="Shop Locator">
+                </div>
+            </div>
+            <div class="col-3 d-flex justify-content-between border">
+                <div>
+                    <h5 class="text-uppercase py-3">subscriptions</h5>
+                </div>
+                <div class="py-2">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="Subscriptions">
                 </div>
             </div>
         </div>
