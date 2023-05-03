09da35060b89f3922bba8eefb20846af71e25be3
DavideAtzo <atzo9477@gmail.com>
2023-05-03 18:31:31 +0200

    inserimento immagine dett. prodotto

--- resources/views/comics_single.blade.php
+++ resources/views/comics_single.blade.php
@@ -1,2 +1,16 @@
 @extends('layouts.app')
-<h2>sono dettaglio prodotto</h2>
+
+@section('page-main')
+<section id="dettaglio_prodotto">
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <div class="img-box">
+                    <img src="{{ $comics_single['thumb'] }}" alt="{{ $comics_single['title'] }}">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+
