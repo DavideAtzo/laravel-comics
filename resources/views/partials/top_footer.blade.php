a7190b97620857b0b47cddd76dffc26211b0515c
DavideAtzo <atzo9477@gmail.com>
2023-05-03 15:27:41 +0200

    top footer nav

--- resources/views/partials/top_footer.blade.php
+++ resources/views/partials/top_footer.blade.php
@@ -1,3 +1,13 @@
 <section>
-    <h1>top footer</h1>
+    <div class="container">
+        <div class="row">
+            <ul class="list-unstyled d-flex justify-content-between align-items-center gap-5 py-3">
+                <li><a href="#">DIGITAL COMICS</a> </li>
+                <li><a href="#">DC MERCHANDISE</a></li>
+                <li><a href="#">SUBSCRIPTION</a></li>
+                <li><a href="#">COMIC SHOP LOCATOR</a></li>
+                <li><a href="#">DC POWER VISA</a></li>
+            </ul>
+        </div>
+    </div>
 </section>
